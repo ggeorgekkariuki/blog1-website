12181aa228ace4b8242965bdbae995d1a80171b2
George Kariuki <gconsummer.gk@gmail.com>
2018-03-19 08:44:03 +0300

    Lesson on testing view Unit/ExampleTest.php

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,19 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+//Creating a post comment(s)
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+
+    return [
+        'user_id' => function() {
+
+            return factory(App\User::class)->create()->id;
+
+        } ,
+
+        'title' => $faker->sentence,
+        
+        'body' => $faker->paragraph
+    ];
+});
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -6,15 +6,59 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use App\Post;
+
 class ExampleTest extends TestCase
 {
+    //This will roll back and undo anything we have done to avoid having
+    //many posts on every phpunit command
+    use DatabaseTransactions;
     /**
      * A basic test example.
      *
      * @return void
      */
+
+     //1. phpunit.xml created a database path and assigned it to the blog_testing database
+     //2. ModelFactory.php created a factory of creating fake posts
+     //3. tests/Unit/ExampleTest.php created this test
+     //4. Executed in vagrant using phpunit tests/Unit/ExampleTest.php
+
+
+     
+
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        //$this->assertTrue(true);
+
+        //Given I have two posts a month apart
+        $first = factory(Post::class) -> create();
+
+        //This post was created a month before
+        $second = factory(Post::class) -> create(
+
+            ['created_at' => \Carbon\Carbon::now()->subMonth()]
+
+        );
+
+        //When I fetch the archives method in the Post Model
+        $posts = Post::archives();
+        //dd($posts);
+        
+        //The response should be in the proper format
+        $this->assertEquals([
+            //The first record
+            [
+                "year" => $first->created_at->format('Y'),
+                "month" => $first->created_at->format('F'),
+                "published" => 2
+            ], 
+            //The second record
+            [
+                "year" => $second->created_at->format('Y'),
+                "month" => $second->created_at->format('F'),
+                "published" => 2
+            ]
+        ] , $posts);
     }
 }
